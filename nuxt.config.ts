90ce61ec8fe1e8f799494ba10c559fc2065d1063
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-21 13:56:57 -0400

    pre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   nitro: {
-    prerender: {
-      routes: ['/'],
-    }
+    hooks: {
+      "prerender:generate"(route) {
+        if (route.route?.includes("private")) {
+          route.skip = true;
+        }
+      },
+    },
   },
   css: ['~/assets/css/main.css'],
   postcss: {
