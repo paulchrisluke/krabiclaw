c1d74575be38d8625cd73460ce1c9a1966a9b965
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-21 14:05:29 -0400

    pre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   nitro: {
+    prerender: {
+      routes: ['/404'],  // Add this line to ensure the 404 page is prerendered
+    },
     hooks: {
       "prerender:generate"(route) {
         if (route.route?.includes("private")) {
