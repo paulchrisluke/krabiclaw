fc3a874457970581c932191ac540f769ccc96d71
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-22 16:04:28 -0400

    generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,11 +28,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  hooks: {
-    'prerender:routes' ({ routes }) {
-      routes.clear() // Do not generate any routes (except the defaults)
-    }
-  },
   postcss: {
     plugins: {
       tailwindcss: {},
