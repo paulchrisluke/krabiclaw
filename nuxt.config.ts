74bbd6b1bafc66cbaae5c45ff6b24893b1fc7138
Joe Elia <44442621+joeelia@users.noreply.github.com>
2024-10-22 11:42:37 -0400

    seo tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,16 +5,16 @@ export default defineNuxtConfig({
       meta: [
         { property: 'og:title', content: 'Sand-O | Authentic Shokupan Bread' },
         { property: 'og:description', content: 'Come taste the 8th wonder of the world. Authentic Shokupan Bread, where dreams come true.' },
-        { property: 'og:image', content: 'https://sand-o-marketing.pages.dev/og-image.jpg' },
-        { property: 'og:video', content: 'https://sand-o-marketing.pages.dev/og-video.mp4' },
-        { property: 'og:url', content: 'https://sand-o-marketing.pages.dev' },
+        { property: 'og:image', content: 'https://sand-o.com/og-image.jpg' },
+        { property: 'og:video', content: 'https://sand-o.com/og-video.mp4' },
+        { property: 'og:url', content: 'https://sand-o.com' },
         { property: 'og:type', content: 'website' },
         { property: 'og:site_name', content: 'Sand-O' },
         { name: 'theme-color', content: '#000000' }
       ],
       link: [
-        { rel: 'icon', href: 'https://sand-o-marketing.pages.dev/favicon.ico', type: 'image/x-icon' },
-        { rel: 'shortcut icon', href: 'https://sand-o-marketing.pages.dev/favicon.ico', type: 'image/x-icon' }
+        { rel: 'icon', href: 'https://sand-o.com/favicon.ico', type: 'image/x-icon' },
+        { rel: 'shortcut icon', href: 'https://sand-o.com/favicon.ico', type: 'image/x-icon' }
       ]
     }
   },
