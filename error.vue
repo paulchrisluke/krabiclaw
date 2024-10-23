eb1144f39a6533eb21e49c6bc6f73f08b48fd188
Dom George <domg93@icloud.com>
2024-10-22 20:05:50 -0400

    commit test

--- error.vue
+++ error.vue
@@ -1,11 +1,13 @@
 <template>
 <NuxtLayout>
+  <div class="flex-1 mx-auto align-center">
     <h1>{{ error.statusCode }}</h1>
     <div class="font-poppins">
-   wrong link sando man!
+   wrong link sando mannnnnnnnnnnnnn!
    <img src="~/assets/images/404.gif" alt="404" />
   </div>
     <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
   </NuxtLayout>
 </template>
 
