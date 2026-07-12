8fc40f93c1e4f9249a84ad6d23f1cd96ab6979ae
Paul Chris Luke <paulchrisluke@users.noreply.github.com>
2026-07-12 11:28:59 +0700

    Scope runtime site config to SEO endpoints

--- server/middleware/zy-site-config.ts
+++ server/middleware/zy-site-config.ts
@@ -2,8 +2,12 @@ import { defineEventHandler, getRequestURL } from 'h3'
 import { updateSiteConfig } from '#site-config/server/composables'
 import { resolveRuntimeSeoSiteConfig } from '~/server/utils/seo-policy'
 
+const SITE_CONFIG_PATHS = new Set(['/robots.txt', '/sitemap.xml'])
+
 export default defineEventHandler((event) => {
   const requestURL = getRequestURL(event)
+  if (!SITE_CONFIG_PATHS.has(requestURL.pathname)) return
+
   const siteConfig = resolveRuntimeSeoSiteConfig({
     tenantType: event.context.tenantType,
     origin: requestURL.origin,
