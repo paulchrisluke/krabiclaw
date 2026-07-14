e4a0360f7e3750dcff770a6dceb4b3d39ceadbbc
Paul Chris Luke <paulchrisluke@users.noreply.github.com>
2026-07-14 10:25:37 +0700

    Replace restaurant-specific shared OG image

---
+++ server/routes/og-image.jpg.get.ts
@@ -0,0 +1,19 @@
+import ogImagePart1 from '~/server/assets/og-image-part-1'
+import ogImagePart2 from '~/server/assets/og-image-part-2'
+
+const OG_IMAGE_BASE64 = `${ogImagePart1}${ogImagePart2}`
+
+export default defineEventHandler((event) => {
+  const binary = atob(OG_IMAGE_BASE64)
+  const image = new Uint8Array(binary.length)
+
+  for (let index = 0; index < binary.length; index += 1) {
+    image[index] = binary.charCodeAt(index)
+  }
+
+  setHeader(event, 'Content-Type', 'image/jpeg')
+  setHeader(event, 'Cache-Control', 'public, max-age=31536000, immutable')
+  setHeader(event, 'Content-Length', String(image.byteLength))
+
+  return image
+})
